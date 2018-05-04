66cead8d428afe0720fe48b5cbce62b1a132a971
TFotakis <fotakistzanis@gmail.com>
2018-05-04 14:52:13 +0300

    ZooKeeper connection implemented on AuthService -  Implemented getStatusText to return json of ZooKeeper's tree and update children nodes

--- WebService/App/zoo.py
+++ WebService/App/zoo.py
@@ -1,4 +1,5 @@
 import json
+import threading
 from django.conf import settings
 from kazoo.client import KazooClient
 from kazoo.security import make_digest_acl
@@ -25,14 +26,28 @@ class Zooconf:
 		dataJsonDict = {
 			'SERVER_HOSTNAME': settings.ZOOKEEPER_HOST,
 			'SERVER_PORT': settings.SERVER_PORT,
-			'SERVER_SCHEME': settings.SERVER_SCHEME,
-			'CONTEXT': settings.CONTEXT
+			'CHILDREN': []
 		}
-		self.__zkcon.create_async(
-			path=settings.ZOOKEEPER_ROOT + settings.ZOOKEEPER_NODE_ID,
-			value=json.JSONEncoder().encode(dataJsonDict).encode(),
-			ephemeral=settings.ZOOKEEPER_NODE_EPHIMERAL
-		)
+		if self.__zkcon.exists(path=settings.ZOOKEEPER_ROOT + settings.ZOOKEEPER_PATH_TO_NODE + settings.ZOOKEEPER_NODE_ID):
+			self.__zkcon.set(
+				path=settings.ZOOKEEPER_ROOT + settings.ZOOKEEPER_PATH_TO_NODE + settings.ZOOKEEPER_NODE_ID,
+				value=json.JSONEncoder().encode(dataJsonDict).encode()
+			)
+		else:
+			self.__zkcon.create_async(
+				path=settings.ZOOKEEPER_ROOT + settings.ZOOKEEPER_PATH_TO_NODE + settings.ZOOKEEPER_NODE_ID,
+				value=json.JSONEncoder().encode(dataJsonDict).encode(),
+				ephemeral=settings.ZOOKEEPER_NODE_EPHIMERAL
+			)
+		if settings.ZOOKEEPER_PATH_TO_NODE != '':
+			data, stat = self.__zkcon.get(settings.ZOOKEEPER_ROOT + settings.ZOOKEEPER_PATH_TO_NODE)
+			dataJsonDict = json.loads(data.decode("utf-8"))
+			if settings.ZOOKEEPER_NODE_ID not in dataJsonDict['CHILDREN']:
+				dataJsonDict['CHILDREN'].append(settings.ZOOKEEPER_NODE_ID)
+			self.__zkcon.set(
+				path=settings.ZOOKEEPER_ROOT + settings.ZOOKEEPER_PATH_TO_NODE,
+				value=json.JSONEncoder().encode(dataJsonDict).encode()
+			)
 
 	def __initFsWatches(self):
 		# lists are supposed to be thread safe in python
@@ -46,12 +61,42 @@ class Zooconf:
 			for child in children:
 				self.__fsList.append(child)
 
-	def getAvailableFs(self):
-		return self.__fsList
+	def getAvailableFs(self): return self.__fsList
 
-	def getZooConnection(self):
-		return self.__zkcon
+	def getZooConnection(self): return self.__zkcon
+
+	def getStatusText(self):
+		result = "{\n"
+		try:
+			rootChildren = self.__zkcon.get_children(settings.ZOOKEEPER_ROOT)
+			for child in rootChildren:
+				data, stat = self.__zkcon.get(settings.ZOOKEEPER_ROOT + child)
+				result += '\t"' + child + '": ' + data.decode("utf-8") + "\n"
+				try:
+					grandchildren = self.__zkcon.get_children(settings.ZOOKEEPER_ROOT + child)
+					for grandchild in grandchildren:
+						data, stat = self.__zkcon.get(settings.ZOOKEEPER_ROOT + child + '/' + grandchild)
+						result += '\t"' + grandchild + '": ' + data.decode("utf-8") + "\n"
+					data, stat = self.__zkcon.get(settings.ZOOKEEPER_ROOT + child + '/')
+					dataJsonDict = json.loads(data.decode("utf-8"))
+					dataJsonDict['CHILDREN'] = grandchildren
+					self.__zkcon.set(
+						path=settings.ZOOKEEPER_ROOT + child,
+						value=json.JSONEncoder().encode(dataJsonDict).encode()
+					)
+				except Exception:
+					pass
+			return result + '}'
+		except Exception:
+			self.__zooConnect()
+			return self.getStatusText()
 
 
-# imported only once per module
+def printit():
+	threading.Timer(300.0, printit).start()
+	print("Heartbeat")
+	print(zk.getStatusText())
+
+
 zk = Zooconf()
+printit()
